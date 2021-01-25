e0c800a53e749e14f5fc050f18b8a92733b800cd
mart8ins <mart8ins@gmail.com>
2021-01-25 20:27:16 +0200

    Finished products edit/update functionality

--- routes/admin.js
+++ routes/admin.js
@@ -11,11 +11,9 @@ const category = require("../seed/category");
 const Product = require("../models/product");
 const Category = require("../models/category");
 
-
 router.get("/products", catchAsync(async (req, res, next) => {
     const categories = await Category.find({}); // to render all added categories to new product form
     const products = await Product.find({}).populate("category");
-    console.log(products)
     res.render("admin/products/products", { categories, products })
 }))
 
@@ -73,14 +71,73 @@ router.post("/products", catchAsync(async (req, res, next) => {
     res.redirect("/manage/products");
 }))
 
-/* update category */
+
+/* update category OR edit product */
 router.patch("/products", catchAsync(async (req, res) => {
-    if (!req.body) throw new AppError("Cant update category, missing request data", 400);
-    const { categoryToChange } = req.body;
+    const { categoryToChange, editProduct } = req.body;
     // change/update category name
-    await Category.findOneAndUpdate({ category: categoryToChange.current }, { category: categoryToChange.new }, { useFindAndModify: false });
+    if (categoryToChange) {
+        await Category.findOneAndUpdate({ category: categoryToChange.current }, { category: categoryToChange.new }, { useFindAndModify: false });
+    }
+
+    // edit product
+    if (editProduct) {
+        // possible new or current products category for update (not update)
+        const cat = await Category.findOne({ category: editProduct.category }).populate("products");
+
+        // product to update ID
+        const { id } = req.query;
+
+        // getting products existing category before updating
+        const productBeforeUpdate = await Product.findById(id).populate("category");
+        const categoryIdforUpdatePurpose = productBeforeUpdate.category._id; // id for category in ref
+        const categoryBeforeUpdate = productBeforeUpdate.category.category; // name for category
+
+        // updates edited product
+        const updatedProduct = await Product.findByIdAndUpdate({ _id: id }, {
+            brand: editProduct.brand,
+            model: editProduct.model,
+            price: {
+                new: editProduct.newPrice,
+                old: editProduct.oldPrice
+            },
+            description: editProduct.description,
+            onSale: editProduct.onSale
+            ,
+            category: cat // reference to category
+        }, { new: true, useFindAndModify: false });
+
+        /* *******************************************
+        if category changes then in old category i need to delete reference to the product, 
+           and add products reference to new category.
+        ***********************************************/
+        const updatedCategoryProductRef = [];
+        if (editProduct.category != categoryBeforeUpdate) {
+            /* adding new product in category product list/products ref if category changes */
+            cat.products.forEach((product) => {
+                if (product._id != id) {
+                    updatedCategoryProductRef.push(product);
+                }
+            });
+            updatedCategoryProductRef.push(updatedProduct);
+            cat.products = updatedCategoryProductRef;
+            cat.save();
+            /* deleting products ref from old category */
+            const categoryToDeleteProductsRef = await Category.findById(categoryIdforUpdatePurpose).populate("products");
+            const allProducts = categoryToDeleteProductsRef.products;
+            const categoryForProductsRefUpdate = [];
+            for (let i of allProducts) {
+                if (i._id != id) {
+                    categoryForProductsRefUpdate.push(i);
+                }
+            }
+            categoryToDeleteProductsRef.products = categoryForProductsRefUpdate;
+            categoryToDeleteProductsRef.save();
+        }
+    }
     res.redirect("/manage/products");
 }))
+
 
 /* delete category */
 /* LATER i NEED TO UPDATE THIS DELETE OPTIONS TO ALSO DELETE associated PRODUCTS for CATEGORY */
@@ -93,6 +150,15 @@ router.delete("/products", catchAsync(async (req, res) => {
     }
     res.redirect("/manage/products");
 }))
+
+// edit specific product
+router.get("/products/:id/edit", catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const categories = await Category.find({});
+    const product = await Product.findById(id).populate("category");
+    res.render("admin/products/edit", { product, categories });
+}))
+
 
 
 
