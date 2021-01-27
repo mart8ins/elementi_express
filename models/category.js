e902d040cf3187f2091e7d24fc8f43a7b995c6d1
mart8ins <mart8ins@gmail.com>
2021-01-27 20:28:26 +0200

    created mongoose middleware when category is deleted then also are deleted every product asociated with current category

--- models/category.js
+++ models/category.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Product = require("../models/product");
 
 const categorySchema = new Schema({
     category: {
@@ -11,5 +12,14 @@ const categorySchema = new Schema({
         { type: Schema.Types.ObjectId, ref: "Product" }
     ]
 })
+
+// mongoose midlleware - when category is deleted then also are deleted all asiociated products with this category
+categorySchema.post("findOneAndDelete", async function (category) {
+    if (category.products.length) {
+        await Product.deleteMany({ _id: { $in: category.products } })
+    }
+    console.log("CATEGORY WITH ALL ASOCIATED PRODUCTS DELETED!!!")
+})
+
 
 module.exports = mongoose.model("Category", categorySchema);
