fbdfc8bc9ce50d7d1dca7b365fef1bcf92c98da4
mart8ins <mart8ins@gmail.com>
2021-02-12 21:40:23 +0200

    fixed registration validation / already existing username or email validation

--- utils/ValidationHandling/registrationValidation.js
+++ utils/ValidationHandling/registrationValidation.js
@@ -1,6 +1,24 @@
 const Joi = require("Joi");
+const User = require("../../models/user");
 
-module.exports = function registrationValidation(req, res, next) {
+module.exports = async function registrationValidation(req, res, next) {
+    const { username, email } = req.body;
+    const ifUserNameExists = await User.findOne({ username });
+    const ifUserEmailExists = await User.findOne({ email });
+
+    // checks if username or email already registred
+    if (ifUserNameExists) {
+        req.flash("error", "Username is already taken!");
+        res.redirect("/auth/register");
+        next();
+    }
+    if (ifUserEmailExists) {
+        req.flash("error", "This email is already registred!");
+        res.redirect("/auth/register");
+        next();
+    }
+
+    // if username and email does not match some existing in db then checks for standart validation
     if (req.body) {
         const registerSchema = Joi.object({
             username: Joi.string().min(3).required(),
