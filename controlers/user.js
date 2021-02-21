620150188c20eb8ed34d2f0c6da2459e83d6a5f3
mart8ins <mart8ins@gmail.com>
2021-02-21 18:29:38 +0200

    cleaned routes/user code

---
+++ controlers/user.js
@@ -0,0 +1,95 @@
+/*****************
+    models
+*******************/
+const User = require("../models/user");
+const Order = require("../models/order");
+
+/*****************
+    async error catching
+*******************/
+const catchAsync = require("../utils/ErrorHandling/catchAsync");
+
+
+
+module.exports.renderLoggedUserProfile = function () {
+    return catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const loggedUser = await User.findById(id);
+        res.render("user/profile", { loggedUser });
+    })
+}
+
+module.exports.createOrEditUserData = function () {
+    return catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const { username, firstName, lastName, email, address, phone } = req.body;
+
+        //if user changes its username or email to what already exists for diferent user then
+        // error info is sent through query
+        const takenUsername = await User.findOne({ username: username });
+        const takenEmail = await User.findOne({ email: email });
+        if (takenUsername && takenUsername._id != id) {
+            res.redirect(`/user/${id}/profile/edit?username_error=true`);
+        }
+        if (takenEmail && takenEmail._id != id) {
+            res.redirect(`/user/${id}/profile/edit?email_error=true`);
+        }
+        const delivery = {
+            firstName,
+            lastName,
+            phone,
+            address
+        }
+        const loggedUser = await User.findById(id);
+
+        loggedUser.username = username;
+        loggedUser.email = email;
+        req.session.user_name = username;
+
+        loggedUser.delivery = delivery;
+        await loggedUser.save();
+
+        res.redirect(`/user/${id}/profile`);
+    })
+}
+
+module.exports.renderEditUserDataPage = function () {
+    return catchAsync(async (req, res) => {
+        const { id } = req.params;
+        const { username_error } = req.query;
+        const { email_error } = req.query;
+        const loggedUser = await User.findById(id);
+        res.render("user/edit", { loggedUser, username_error, email_error });
+    })
+}
+
+module.exports.renderUserOrderHistory = function () {
+    return catchAsync(async (req, res) => {
+        const { id } = req.params;
+        let allOrders = await Order.find({}).populate("user");
+
+        // all users orders
+        let userOrders = [];
+        for (let order in allOrders) {
+            if (allOrders[order].user) {
+                if (allOrders[order].user._id == id) {
+                    let ord = {
+                        products: [],
+                        date: allOrders[order].date,
+                        statusHistory: allOrders[order].statusHistory,
+                        currentStatus: allOrders[order].currentStatus,
+                        totalQ: allOrders[order].order.cartTotals.quantity,
+                        totalSum: allOrders[order].order.cartTotals.price,
+                        delivery: allOrders[order].shipping,
+                        orderNumber: allOrders[order].orderNumber
+                    }
+                    for (let pr in allOrders[order].order.products) {
+                        ord.products.push(allOrders[order].order.products[pr])
+                    }
+                    userOrders.unshift(ord)
+                }
+            }
+        }
+        res.render("user/orders", { userOrders });
+    })
+}
--- routes/user.js
+++ routes/user.js
@@ -1,99 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { loggedUserRouteGuard, loggedUserManageGuard } = require("../utils/Middleware/routeGuarding");
-const User = require("../models/user");
-const Order = require("../models/order");
 
-
+const { renderLoggedUserProfile, createOrEditUserData, renderEditUserDataPage, renderUserOrderHistory } = require("../controlers/user");
 
 /*****************
     ROUTE GUARD FOR LOGGED USERS
 *******************/
 router.use(loggedUserRouteGuard);
 
-/*****************
-    error handling
-*******************/
-const AppError = require("../utils/ErrorHandling/AppError");
-const catchAsync = require("../utils/ErrorHandling/catchAsync");
-
 /* PROFILE INFO AND SETUP */
-router.get("/:id/profile", loggedUserManageGuard, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const loggedUser = await User.findById(id);
-    res.render("user/profile", { loggedUser });
-}))
+router.route("/:id/profile")
+    .get(loggedUserManageGuard, renderLoggedUserProfile())
+    .post(loggedUserManageGuard, createOrEditUserData())
 
-router.get("/:id/profile/edit", loggedUserManageGuard, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const { username_error } = req.query;
-    const { email_error } = req.query;
-    const loggedUser = await User.findById(id);
-
-    res.render("user/edit", { loggedUser, username_error, email_error });
-}))
-
-router.post("/:id/profile", loggedUserManageGuard, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const { username, firstName, lastName, email, address, phone } = req.body;
-
-    //if user changes its username or email to what already exists for diferent user then
-    // error info is sent through query
-    const takenUsername = await User.findOne({ username: username });
-    const takenEmail = await User.findOne({ email: email });
-    if (takenUsername && takenUsername._id != id) {
-        res.redirect(`/user/${id}/profile/edit?username_error=true`);
-    }
-    if (takenEmail && takenEmail._id != id) {
-        res.redirect(`/user/${id}/profile/edit?email_error=true`);
-    }
-    const delivery = {
-        firstName,
-        lastName,
-        phone,
-        address
-    }
-    const loggedUser = await User.findById(id);
-
-    loggedUser.username = username;
-    loggedUser.email = email;
-    req.session.user_name = username;
-
-    loggedUser.delivery = delivery;
-    await loggedUser.save();
-
-    res.redirect(`/user/${id}/profile`);
-}))
+router.route("/:id/profile/edit")
+    .get(loggedUserManageGuard, renderEditUserDataPage())
 
 
 /* USERS ORDER HISTORY */
-router.get("/:id/orders", loggedUserManageGuard, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    let allOrders = await Order.find({}).populate("user");
-
-    // all users orders
-    let userOrders = [];
-    for (let order in allOrders) {
-        if (allOrders[order].user) {
-            if (allOrders[order].user._id == id) {
-                let ord = {
-                    products: [],
-                    date: allOrders[order].date,
-                    statusHistory: allOrders[order].statusHistory,
-                    currentStatus: allOrders[order].currentStatus,
-                    totalQ: allOrders[order].order.cartTotals.quantity,
-                    totalSum: allOrders[order].order.cartTotals.price,
-                    delivery: allOrders[order].shipping,
-                    orderNumber: allOrders[order].orderNumber
-                }
-                for (let pr in allOrders[order].order.products) {
-                    ord.products.push(allOrders[order].order.products[pr])
-                }
-                userOrders.unshift(ord)
-            }
-        }
-    }
-    res.render("user/orders", { userOrders });
-}))
+router.route("/:id/orders")
+    .get(loggedUserManageGuard, renderUserOrderHistory())
 
 module.exports = router;
