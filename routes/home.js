9dd74567ef380984f85a9075fea1715315bdcce4
mart8ins <mart8ins@gmail.com>
2021-02-22 20:13:48 +0200

    improved landing page, created avout us page and added info

--- routes/home.js
+++ routes/home.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/ErrorHandling/catchAsync");
-const Category = require("../models/category");
 
 
 
 // landing page, with all categories and products
 router.get("/", catchAsync(async (req, res) => {
     res.render("index")
+}))
+
+router.get("/about", catchAsync(async (req, res) => {
+    res.render("about")
 }))
 
 module.exports = router;
